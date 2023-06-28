36256309b4c2edee69f4a28d60357799b2b77484
Hangyan Zhu <33981458+HyZhu39@users.noreply.github.com>
2023-06-28 21:09:18 +0800

    Delete basicsr directory

--- basicsr/metrics/__init__.py
+++
@@ -1,7 +0,0 @@
-from .niqe import calculate_niqe
-from .psnr_ssim import calculate_psnr, calculate_ssim
-from .lpips_metric import calculate_lpips
-from .UDC_mat_psnr_ssim import calculate_psnr_mat, calculate_ssim_mat
-from .UDC_match import calculate_psnr_match, calculate_ssim_match, calculate_lpips_match
-
-__all__ = ['calculate_psnr', 'calculate_ssim', 'calculate_niqe', 'calculate_lpips', 'calculate_psnr_mat','calculate_ssim_mat','calculate_psnr_match','calculate_ssim_match','calculate_lpips_match']
--- basicsr/version.py
+++
@@ -1,5 +0,0 @@
-# GENERATED VERSION FILE
-# TIME: Sat May 20 10:26:10 2023
-__version__ = '1.1.0+1.4.2'
-short_version = '1.1.0'
-version_info = (1, 1, 0)
